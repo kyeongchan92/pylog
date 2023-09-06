81584e23e437cd21e3995f77f0dcebfd91d64624
kyeongchan92 <nahcklee@gmail.com>
2023-09-07 00:00:59 +0900

    119p부터

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from blog.views import post_list
 from config.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('posts/', post_list)
 ]
